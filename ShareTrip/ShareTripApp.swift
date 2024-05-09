3d08e16fe0575351774345aae4886e7a91d978d6
Jordi Aragones-Vilella <jordi.aragones@gmail.com>
2024-05-09 09:08:33 +0200

    Initial Commit

---
+++ ShareTrip/ShareTripApp.swift
@@ -0,0 +1,20 @@
+//
+//  ShareTripApp.swift
+//  ShareTrip
+//
+//  Created by Jordi Aragones Vilella on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ShareTripApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
