27b7c23aed0efcc39c65024c4e9f2984bf20db47
Jordi Aragones-Vilella <jordi.aragones@gmail.com>
2025-09-30 02:00:37 +0200

    Update ShareTripUITests.swift

--- ShareTripUITests/ShareTripUITests.swift
+++ ShareTripUITests/ShareTripUITests.swift
@@ -24,6 +24,8 @@ final class ShareTripUITests: XCTestCase {
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        let app = XCUIApplication()
+        app.terminateWithError()
     }
 
     func testExample() throws {
