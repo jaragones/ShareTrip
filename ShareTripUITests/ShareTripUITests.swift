1d8e479018da112a4719e2e27e7143bde90c3bc1
Jordi Aragones-Vilella <jordi.aragones@gmail.com>
2025-10-12 04:18:58 +0200

    Update ShareTripUITests.swift

--- ShareTripUITests/ShareTripUITests.swift
+++ ShareTripUITests/ShareTripUITests.swift
@@ -34,6 +34,14 @@ final class ShareTripUITests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
+    func testExampl2e() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
