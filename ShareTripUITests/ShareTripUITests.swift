c81ed2eaf52543a216fcab0b0330f18fbce7e6be
Jordi Aragones-Vilella <jordi.aragones@gmail.com>
2025-11-13 16:25:47 +0100

    ...

--- ShareTripUITests/ShareTripUITests.swift
+++ ShareTripUITests/ShareTripUITests.swift
@@ -42,19 +42,19 @@ final class ShareTripUITests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
-//    func testCoffeeIsEssential() {
-//        let needsCoffee = true
-//        XCTAssertTrue(needsCoffee, "☕️ Developer productivity critically depends on coffee.")
-//    }
-//    
-//    func testTeaIsEssential() {
-//        let needsCoffee = true
-//        XCTAssertTrue(needsCoffee, "☕️ Developer productivity critically depends on coffee.")
-//    }
-//    
-//    func testPhysicsStillApplies() {
-//        XCTAssertEqual(2 + 2, 4, "🚨 Math is broken. Send help.")
-//    }
+    func testCoffeeIsEssential2() {
+        let needsCoffee = true
+        XCTAssertTrue(needsCoffee, "☕️ Developer productivity critically depends on coffee.")
+    }
+    
+    func testTeaIsEssential2() {
+        let needsCoffee = true
+        XCTAssertTrue(needsCoffee, "☕️ Developer productivity critically depends on coffee.")
+    }
+    
+    func testPhysicsStillApplies2() {
+        XCTAssertEqual(2 + 2, 4, "🚨 Math is broken. Send help.")
+    }
 
     func testCokeIsEssential() {
         let needsCocaCola = true
