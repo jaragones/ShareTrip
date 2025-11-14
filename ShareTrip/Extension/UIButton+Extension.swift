b1c8792856855854d5bc0b5819d62041e610a778
Jordi Aragones-Vilella <jordi.aragones@gmail.com>
2025-11-14 10:55:26 +0100

    ...

---
+++ ShareTrip/Extension/UIButton+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  UIView+Extension.swift
+//  ShareTrip
+//
+//  Created by Jordi Aragones Vilella on 14/11/25.
+//
+
+import UIKit
+
+extension UIButton {
+
+    func setSubtitle(subtitle: String) {
+        print("\(subtitle)")
+    }
+}
