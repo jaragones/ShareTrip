17a94734cc61d23179dec3d9ce02d4097d8708c7
Jordi Aragones-Vilella <jordi.aragones@gmail.com>
2025-11-14 11:31:38 +0100

    ...

---
+++ ShareTrip/Extension/UITextField+Extension 2.swift
@@ -0,0 +1,15 @@
+//
+//  UIView+Extension.swift
+//  ShareTrip
+//
+//  Created by Jordi Aragones Vilella on 14/11/25.
+//
+
+import UIKit
+
+extension UITextField {
+
+    func setSubtitle(subtitle: String) {
+        print("\(subtitle)")
+    }
+}
