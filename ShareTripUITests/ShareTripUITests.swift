62a73ea7efb2527badef5ab0227585cba25f9637
Jordi Aragones-Vilella <jordi.aragones@gmail.com>
2025-11-16 00:31:30 +0100

    ...

--- ShareTripUITests/ShareTripUITests.swift
+++ ShareTripUITests/ShareTripUITests.swift
@@ -61,12 +61,12 @@ final class ShareTripUITests: XCTestCase {
 //        XCTAssertTrue(needsCocaCola, "☕️ Developer productivity critically depends on coffee.")
 //    }
     
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+//    func testLaunchPerformance() throws {
+//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+//            // This measures how long it takes to launch your application.
+//            measure(metrics: [XCTApplicationLaunchMetric()]) {
+//                XCUIApplication().launch()
+//            }
+//        }
+//    }
 }
