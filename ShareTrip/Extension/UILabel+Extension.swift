25564ec0fcd789c0907f5e6d815efd1a57da0b6b
Jordi Aragones-Vilella <jordi.aragones@gmail.com>
2025-11-16 03:21:14 +0100

    ...

---
+++ ShareTrip/Extension/UILabel+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  UILabel+Extension.swift
+//  ShareTrip
+//
+//  Created by Jordi Aragones Vilella on 16/11/25.
+//
+
+import UIKit
+
+extension UILabel {
+    
+    // MARK: - Layout
+    
+    func setTitle() {
+        print()
+    }
+}
