5168cf437f0bd97ffc8f07fc847fd8ddd573ea1c
Jordi Aragones-Vilella <jordi.aragones@gmail.com>
2025-11-16 22:55:25 +0100

    Added new method coffee

--- ShareTripUITests/ShareTripUITests.swift
+++ ShareTripUITests/ShareTripUITests.swift
@@ -25,7 +25,7 @@ final class ShareTripUITests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-//    
+  
     func testExample() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
@@ -42,10 +42,10 @@ final class ShareTripUITests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
-//    func testCoffeeIsEssentialResync() {
-//        let needsCoffee = true
-//        XCTAssertTrue(needsCoffee, "☕️ Developer productivity critically depends on coffee.")
-//    }
+    func testCoffeeIsEssentialResync() {
+        let needsCoffee = true
+        XCTAssertTrue(needsCoffee, "☕️ Developer productivity critically depends on coffee.")
+    }
 //    
 //    func testTeaIsEssential() {
 //        let needsCoffee = true
