6d96eb867a0293e08676f003eca7f083eda5f748
Jordi Aragones-Vilella <jordi.aragones@gmail.com>
2025-11-19 15:56:02 +0100

    remove 3 functions and add one

--- ShareTripUITests/ShareTripUITests.swift
+++ ShareTripUITests/ShareTripUITests.swift
@@ -25,7 +25,7 @@ final class ShareTripUITests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-//    
+   
     func testExample() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
@@ -34,27 +34,31 @@ final class ShareTripUITests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testExamplificationResync() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testCoffeeIsEssentialResync() {
-        let needsCoffee = true
-        XCTAssertTrue(needsCoffee, "☕️ Developer productivity critically depends on coffee.")
-    }
-    
-    func testTeaIsEssential() {
-        let needsCoffee = true
-        XCTAssertTrue(needsCoffee, "☕️ Developer productivity critically depends on coffee.")
-    }
-//    
-//    func testPhysicsStillApplies() {
-//        XCTAssertEqual(2 + 2, 4, "🚨 Math is broken. Send help.")
+///////////////
+//
+//    func testExamplificationResync() throws {
+//        // UI tests must launch the application that they test.
+//        let app = XCUIApplication()
+//        app.launch()
+//
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
 //    }
+    
+//    func testCoffeeIsEssentialResync() {
+//        let needsCoffee = true
+//        XCTAssertTrue(needsCoffee, "☕️ Developer productivity critically depends on coffee.")
+//    }
+//    
+//    func testTeaIsEssential() {
+//        let needsCoffee = true
+//        XCTAssertTrue(needsCoffee, "☕️ Developer productivity critically depends on coffee.")
+//    }
+//
+///////////////
+    
+    func testPhysicsStillApplies() {
+        XCTAssertEqual(2 + 2, 4, "🚨 Math is broken. Send help.")
+    }
 //
 //    func testCokeIsEssential() {
 //        let needsCocaCola = true
