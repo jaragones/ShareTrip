d9692d063b1edccf0b16f92269b50f242eac9be6
Jordi Aragones-Vilella <jordi.aragones@gmail.com>
2025-11-20 00:30:45 +0100

    adding new file

---
+++ ShareTrip/Extension/UILabel+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  UILabel+Extension.swift
+//  ShareTrip
+//
+//  Created by Jordi Aragones Vilella on 20/11/25.
+//
+
+import UIKit
+
+extension UILabel {
+
+    func labelDescription() {
+        print("Some custom description that can be query")
+    }
+}
