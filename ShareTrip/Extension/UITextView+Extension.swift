55fda2284a29cdaed17ef831c4fcf4a4f6dc5560
Jordi Aragones-Vilella <jordi.aragones@gmail.com>
2025-11-20 15:36:10 +0100

    Added new uitextview extension

---
+++ ShareTrip/Extension/UITextView+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UITextView+Extension.swift
+//  ShareTrip
+//
+//  Created by Jordi Aragones Vilella on 20/11/25.
+//
+
+import UIKit
+
+extension UITextView {
+    func descriptionForContent() {
+        print("Some cool stuff")
+    }
+}
